d4615fbd9d25a044ac6fbe50a1af64a68ce463da
nbernot <165718024+nbernot@users.noreply.github.com>
2024-04-02 17:33:37 -0400

    Made the instance varibles, constructors, and get/set methods for image

--- A6-Template/src/Image.java
+++ A6-Template/src/Image.java
@@ -1,6 +1,39 @@
+import java.awt.*;
+
 abstract class Image {
+    private int width;
+    private int height;
+    private Color[][] pixels;
 
     public Image(int width, int height) {
-       // TODO.
+       this.width = width;
+       this.height = height;
+       this.pixels = new Color[width][height];
     }
+    public Image(){
+        this(0,0);
+    }
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public Color[][] getPixels(){
+        return pixels;
+    }
+    public void setWidth(int width){
+        this.width = width;
+    }
+    public void setHeight(int height){
+        this.height = height;
+    }
+    public void setPixels(Color[][] pixels){
+        this.pixels = pixels;
+    }
+
+
+
+
+
 }
