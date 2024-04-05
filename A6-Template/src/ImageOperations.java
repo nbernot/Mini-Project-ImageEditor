96a4464fdec48640c6212b7a342c875a6b512599
aagawrys01 <aagawrys@gmail.com>
2024-04-04 22:36:50 -0400

    added methods to ImageOperations class

--- A6-Template/src/ImageOperations.java
+++ A6-Template/src/ImageOperations.java
@@ -3,4 +3,35 @@ class ImageOperations {
     public static void main(String[] args) {
         // TODO.
     }
+
+
+    public static Image zeroRed(Image img){
+        // TODO.
+    }
+
+
+    public static Image grayscale(Image img){
+        // TODO.
+    }
+
+
+    public static Image invert(Image img){
+        // TODO.
+    }
+
+
+    public static Image crop(Image img, int x1, int y1, int w, int h){
+        // TODO.
+    }
+
+
+    public static Image mirror(Image img, String mode){
+        // TODO.
+    }
+
+
+    public static Image repeat(Image img, int n, String dir){
+        // TODO.
+    }
+
 }
