82e8fcbf75cf356ca480bbae8bfcfddd99e042bf
aagawrys01 <aagawrys@gmail.com>
2024-04-11 10:40:45 -0400

    completed imageOP class

--- A6-Template/src/ImageOperations.java
+++ A6-Template/src/ImageOperations.java
@@ -13,7 +13,6 @@ class ImageOperations {
             for (int j = 0; j < img.getWidth(); j++) {
                 Color original = holder[i][j];
                 holder[i][j] = new Color(0, original.getGreen(), original.getBlue());
-
             }
         }
         return img;
@@ -25,9 +24,8 @@ class ImageOperations {
         for (int i = 0; i < img.getHeight(); i++) {
             for (int j = 0; j < img.getWidth(); j++) {
                 Color originalColor = holder[i][j];
-                int grayLevel = (originalColor.getRed() + originalColor.getGreen() + originalColor.getBlue()) / 3;
-                holder[i][j] = new Color(grayLevel, grayLevel, grayLevel);
-
+                int gray = (originalColor.getRed() + originalColor.getGreen() + originalColor.getBlue()) / 3;
+                holder[i][j] = new Color(gray, gray, gray);
             }
         }
         return img;
@@ -40,7 +38,6 @@ class ImageOperations {
             for (int j = 0; j < img.getWidth(); j++) {
                 Color originalColor = holder[i][j];
                 holder[i][j] = new Color(255 - originalColor.getRed(), 255 - originalColor.getGreen(), 255 - originalColor.getBlue());
-
             }
         }
         return img;
@@ -48,17 +45,60 @@ class ImageOperations {
 
 
     public static Image crop(Image img, int x1, int y1, int w, int h) {
-        // TODO.
+        Image croppedImage = new PpmImage(w, h);
+        Color[][] sourceColors = img.getColors();
+        Color[][] croppedColors = croppedImage.getColors();
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (i + y1 < img.getHeight() && j + x1 < img.getWidth()) {
+                    croppedColors[i][j] = sourceColors[i + y1][j + x1];
+                } else {
+                    croppedColors[i][j] = new Color(0, 0, 0);
+                }
+            }
+        }
+        return croppedImage;
     }
 
 
     public static Image mirror(Image img, String mode) {
-        // TODO.
+        Color[][] colors = img.getColors();
+        int width = img.getWidth();
+        int height = img.getHeight();
+        Image mirroredImage = new PpmImage(width, height);
+        Color[][] mirroredColors = mirroredImage.getColors();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ("H".equals(mode)) {
+                    mirroredColors[i][j] = colors[i][width - 1 - j];
+                } else if ("V".equals(mode)) {
+                    mirroredColors[i][j] = colors[height - 1 - i][j];
+                }
+            }
+        }
+        return mirroredImage;
     }
 
 
     public static Image repeat(Image img, int n, String dir) {
-        // TODO.
+        int width = img.getWidth();
+        int height = img.getHeight();
+        if ("H".equals(dir)) {
+            width *= n;
+        } else if ("V".equals(dir)) {
+            height *= n;
+        }
+        Image repeatImage = new PpmImage(width, height);
+        Color[][] sourceColors = img.getColors();
+        Color[][] repeatedColors = repeatImage.getColors();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int sourceJ = j % img.getWidth();
+                int sourceI = i % img.getHeight();
+                repeatedColors[i][j] = sourceColors[sourceI][sourceJ];
+            }
+        }
+        return repeatImage;
     }
 
 }
