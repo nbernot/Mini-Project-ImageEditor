f1f4ab3e5511bb9b51c40ba1070f6b8b929746a4
aagawrys01 <aagawrys@gmail.com>
2024-04-11 09:22:03 -0400

    PPm image output

--- A6-Template/src/PpmImage.java
+++ A6-Template/src/PpmImage.java
@@ -1,4 +1,8 @@
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 class PpmImage extends Image {
@@ -8,36 +12,54 @@ class PpmImage extends Image {
         super(width, height);
     }
 
-    public PpmImage(String filename){
+    public PpmImage(String filename) {
         super();
         readImage(filename);
     }
 
 
-    void readImage(String filename){
-        // OPEN the scanner. CATCH THE EXPECTION! DO NOT
-        // SIMPLY MARK THE METHOD AS "throws" IOEXception"
-        Scanner sc = new Scanner(____________);
-        // SKIP the P3 line.
-        sc.nextLine();
-        int width = sc.nextInt();
-        int height = sc.nextInt();
-        this.setWidth(width);
-        this.setHeight(height);
-        this.setColors(new Color[______][______]);
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                // Populate the pixels array
+    void readImage(String filename) {
+        try {
+            // OPEN the scanner. CATCH THE EXPECTION! DO NOT
+            // SIMPLY MARK THE METHOD AS "throws" IOEXception"
+            Scanner sc = new Scanner(new File(filename));
+            // SKIP the P3 line.
+            sc.nextLine();
+            int width = sc.nextInt();
+            int height = sc.nextInt();
+            this.setWidth(width);
+            this.setHeight(height);
+            this.setColors(new Color[width][height]);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    // Populate the pixels array
+                    int red = sc.nextInt();
+                    int green = sc.nextInt();
+                    int blue = sc.nextInt();
+                    this.getColors()[i][j] = new Color(red, green, blue);
+                }
             }
+            sc.close();
+        } catch (IOException e) {
+            e.getMessage();
         }
     }
 
     @Override
     public void output(String filename) {
-
+        try (PrintWriter write = new PrintWriter(filename)) {
+            write.println("P3");
+            write.println(getWidth() + " " + getHeight());
+            write.println(255);
+            for (Color[] row : getColors()) {
+                for (Color pixel : row) {
+                    write.println(pixel.getRed() + " " + pixel.getGreen() + " " + pixel.getBlue());
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error writing to file: " + filename);
+        }
     }
-
-
 
 
 }
